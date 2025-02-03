66f94aa0861b1ad34808ebae3f074c94e81a1420
Daniel Morgan <>
2025-02-03 02:54:13 -0300

    Se implementa logica de reintentos al invocar mock service de porcentaje dinamico

--- rest-api-challenge/src/main/java/cl/tenpo/rest/api/challenge/config/ExternalPercentageFeignClientConfiguration.java
+++ rest-api-challenge/src/main/java/cl/tenpo/rest/api/challenge/config/ExternalPercentageFeignClientConfiguration.java
@@ -1,9 +1,16 @@
 package cl.tenpo.rest.api.challenge.config;
 
+import feign.Logger;
 import feign.Request;
+import feign.RetryableException;
+import feign.Retryer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class ExternalPercentageFeignClientConfiguration {
@@ -11,9 +18,21 @@ public class ExternalPercentageFeignClientConfiguration {
     private int connectTimeout = 2000;
     @Value("${feign.client.config.externalPercentageClient.readTimeout}")
     private int readTimeout = 2000;
+    @Value("${feign.client.config.externalPercentageClient.retries}")
+    private int retries = 5;
 
     @Bean
     public Request.Options options() {
         return new Request.Options(connectTimeout, readTimeout); // 5 seconds connect, 10 seconds read
+    }
+
+    @Bean
+    public Retryer retryer(ApplicationContext applicationContext, Environment env) {
+        return new Retryer.Default(100L, TimeUnit.SECONDS.toMillis(1L), this.retries);
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
